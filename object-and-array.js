75bf3155b04695ed339ed4ac3f6d474fd6eb4c34
emmikapk-bit <emmika.pk@gmail.com>
2026-03-06 14:46:18 +0700

    added-js

---
+++ object-and-array.js
@@ -0,0 +1,11 @@
+const laerner = {
+    name: "Emmi",
+    age : "31",
+    isGraduated : false,
+    hobbies : ["Watch horror or comedy movies.","listen to music.","Sitting lost in thought."]
+};
+
+console .log("Name :",laerner.name);
+console .log("Age :",laerner.age);
+console .log("isGraduat :",laerner.isGraduated);
+console .log("hobbies :",laerner.hobbies [0]);
